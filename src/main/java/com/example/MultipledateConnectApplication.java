6b0b014f7eab1ecd2b0d636a25f8c4ebe3e1b05c
Pavan0229 <bugganapavan@gmail.com>
2022-08-14 21:30:26 +0530

    added MultipleDataBase

---
+++ src/main/java/com/example/MultipledateConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipledateConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipledateConnectApplication.class, args);
+	}
+
+}
